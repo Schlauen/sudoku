b7201b26661025111d57e5ee52e4dfa096de03ff
LauenWu <l_wuergler@outlook.com>
2024-02-12 13:34:04 +0100

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use wasm_bindgen::prelude::*;
+use array2d::Array2D;
+use bitvec::prelude::*;
+use rand::prelude::*;
 
 const FIELDS:[(u8, u8); 81] = [
     (0,0), (0,1), (0,2), (0,3), (0,4), (0,5), (0,6), (0,7), (0,8),
@@ -38,32 +41,42 @@ const VALUES_BIN_INV:[u16;9] = [
 // if changed, apply 'wasm-pack build'
 #[wasm_bindgen]
 pub struct PlayfieldState {
-    values: Vec<u8>,
+    values: Array2D<u8>,
+    errors: Array2D<bool>,
+    solution: Option<Array2D<u8>>,
+    fixed: Array2D<bool>,
+    poss_rows: [u16; 9],
+    poss_cols: [u16; 9],
+    poss_quads: [u16; 9],
+    solved: bool,
+    show_errors: bool,
+    status_text: String,
+    difficulty: f32,
 }
 
 #[wasm_bindgen]
 impl PlayfieldState {
     pub fn new() -> PlayfieldState {
-        let mut values = Vec::new();
-        (0..81).for_each(|i| values.push((i % 9) + 1));
-        PlayfieldState { values }
+        PlayfieldState { 
+            values: Array2D::filled_with(0, 9, 9),
+            errors: Array2D::filled_with(false, 9, 9),
+            solution: Option::None,
+            fixed: Array2D::filled_with(false, 9, 9),
+            poss_rows: [0b1111111111111111u16; 9],
+            poss_cols: [0b1111111111111111u16; 9],
+            poss_quads: [0b1111111111111111u16; 9],
+            solved: false,
+            show_errors: false,
+            status_text: format!(""),
+            difficulty: 40.0,
+        }
     }
 
-    pub fn set_value(&mut self, value:u8, i:usize) {
-        self.values[i] = value;
+    pub fn set_value(&mut self, value:u8, row:usize, col:usize) {
+        self.values[(row, col)] = value;
     }
 
-    pub fn values(&self) -> *const u8 {
-        self.values.as_ptr()
+    pub fn get_value(&self, row:usize, col:usize) -> u8 {
+        self.values[(row, col)]
     }
-}
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
 }
